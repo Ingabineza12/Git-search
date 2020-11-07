f34ad0623bf2a819a25d2a6bebb3c93c20b4392a
Deborah M Ingabire <ingabineza@gmail.com>
2020-11-07 13:48:30 +0200

    modified user class

--- src/app/user-class/user.ts
+++ src/app/user-class/user.ts
@@ -1,2 +1,10 @@
 export class User {
+    constructor(public name:string,
+         public location:string,
+         public followers: string,
+         public following:string,
+         public html_url:string,
+         public created_at:Date,
+         public  avatar_url:string){}
+
 }
