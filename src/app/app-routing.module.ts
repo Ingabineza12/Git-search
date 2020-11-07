b23980d17865664a21fdc85cc913772a7c2cec2b
Deborah M Ingabire <ingabineza@gmail.com>
2020-11-07 13:53:30 +0200

    modified app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GitComponent } from './git/git.component';
+import { AboutComponent } from './about/about.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'git', component: GitComponent},
+  { path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
